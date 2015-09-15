cf26fb4b076b3c88839c6ae7058b00870d1f0e30
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-15 18:59:02 -0300

    Cria o teste para a data de criacao e data da movimentacao

--- src/br/com/caelum/financas/modelo/Movimentacao.java
+++ src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -1,5 +1,8 @@
 package br.com.caelum.financas.modelo;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,14 +11,30 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@Table(name = "Movimentacao", schema = "controlefinancas")
+@Table(name = "Movimentacao")
 @Entity
 public class Movimentacao {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
+	@Column(name = "descricao", length = 100)
+	private String descricao;
+	
+	@Column(name = "valor")
+	private BigDecimal valor;
+	
+	@Column(name = "data")
+//	@Temporal(value = TemporalType.TIME) Nao podemos persistir somente TIME
+	@Temporal(value = TemporalType.DATE)
+	private Calendar data;
+	
+	@Column(name = "data_criacao") //Se nao usarmos @Temporal é salvo o DateTime por padrão
+	private Calendar dataCricao = Calendar.getInstance();
 
 	@Enumerated(value = EnumType.STRING)
 	@Column(name = "tipo_movimentacao")
@@ -25,11 +44,14 @@ public class Movimentacao {
 	@Column(name = "tipo_atendimento")
 	private TipoAtendimento tipoAtendimento;
 	
-	public Movimentacao(Long id, TipoMovimentacao tipoMovimentacao,
-			TipoAtendimento tipoAtendimento) {
+	public Movimentacao(Long id, String descricao, TipoMovimentacao tipoMovimentacao,
+			TipoAtendimento tipoAtendimento, BigDecimal valor, Calendar data) {
 		if (id != null) {
 			this.id = id;
 		}
+		this.data = data;
+		this.valor = valor;
+		this.descricao = descricao;
 		this.tipoMovimentacao = tipoMovimentacao;
 		this.tipoAtendimento = tipoAtendimento;
 	}
@@ -63,6 +85,18 @@ public class Movimentacao {
 
 	public void setTipoAtendimento(TipoAtendimento tipoAtendimento) {
 		this.tipoAtendimento = tipoAtendimento;
+	}
+	
+	public BigDecimal getValor() {
+		return valor;
+	}
+	
+	public Calendar getData() {
+		return data;
+	}
+
+	public Calendar getDataCricao() {
+		return dataCricao;
 	}
 	
 }
--- src/br/com/caelum/financas/modelo/crud/movimentacao/TestaInsereMovimentacaoComTipoMovimentacao.java
+++ src/br/com/caelum/financas/modelo/crud/movimentacao/TestaInsereMovimentacaoComTipoMovimentacao.java
@@ -1,5 +1,8 @@
 package br.com.caelum.financas.modelo.crud.movimentacao;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 
 import br.com.caelum.financas.infra.JPAUtil;
@@ -11,7 +14,8 @@ public class TestaInsereMovimentacaoComTipoMovimentacao {
 
 	public static void main(String[] args) {
 		EntityManager manager = new JPAUtil().getEntityManager();
-		Movimentacao movimentacao = new Movimentacao(null, TipoMovimentacao.ENTRADA, TipoAtendimento.PREMIUM);
+		Calendar hoje = Calendar.getInstance();
+		Movimentacao movimentacao = new Movimentacao(null, "Cerveja", TipoMovimentacao.ENTRADA, TipoAtendimento.PREMIUM, new BigDecimal(100), hoje);
 		
 		manager.getTransaction().begin();
 		manager.persist(movimentacao);
